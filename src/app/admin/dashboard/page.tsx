b2833503fb362a846dfe1d7d7e127daa6c6914f0
Nur Khoerudin <bangudin912@gmail.com>
2026-08-12 15:20:27 +0700

    Fix dashboard page

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,19 +1,19 @@
 import DashboardStats from "@/app/admin/components/DashboardStats";
 
 export default function DashboardPage() {
-    return (
-        <main className="space-y-6">
-            <div>
-                <h1 className="text-2xl font-bold tracking-tight">
-                    Dashboard
-                </h1>
+  return (
+    <main className="space-y-6">
+      <div>
+        <h1 className="text-2xl font-bold tracking-tight">
+          Dashboard
+        </h1>
 
-                <p className="text-muted-foreground">
-                    Selamat datang di halaman dashboard admin.
-                </p>
-            </div>
+        <p className="text-muted-foreground">
+          Selamat datang di halaman dashboard admin.
+        </p>
+      </div>
 
-            <DashboardStats />
-        </main>
-    );
+      <DashboardStats />
+    </main>
+  );
 }
